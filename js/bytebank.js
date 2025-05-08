6af93dc8ebe4e6999dd0cdcdb0a8a6f1b7b3d027
Camila Santiago <92113919+Camilay3@users.noreply.github.com>
2025-05-08 11:46:13 -0300

    Aula 06
    Incluindo o Typescript no código

--- js/bytebank.js
+++ js/bytebank.js
@@ -1,43 +1,38 @@
-let saldo = 3000;
-
-const elementoSaldo = document.querySelector('.saldo-valor .valor');
-const elementoFormulario = document.querySelector('.block-nova-transacao form');
-
-elementoSaldo.textContent = saldo;
-elementoFormulario.addEventListener('submit', function(event) {
+var saldo = 3000;
+var elementoSaldo = document.querySelector('.saldo-valor .valor'); // Converte o valor para que o código saiba que se trata de um elemento HTML
+var elementoFormulario = document.querySelector('.block-nova-transacao form');
+if (elementoSaldo != null) { // Garante que o elemento existe no código
+    elementoSaldo.textContent = saldo.toString();
+}
+elementoFormulario.addEventListener('submit', function (event) {
     event.preventDefault(); // Bloqueia o comportamento padrão de recarregar a página ao enviar o formulário
-
     if (!elementoFormulario.checkValidity()) {
         alert('Por favor, preeencha todos os campos da transação!');
-        return
+        return;
     }
-
-    const inputTipoTransacao = elementoFormulario.querySelector('#tipoTransacao');
-    const inputValor = elementoFormulario.querySelector('#valor');
-    const inputData = elementoFormulario.querySelector('#data');
-
-    let tipoTransacao = inputTipoTransacao.value;
-    let valor = inputValor.value;
-    let data = inputData.value; // ou elementoFormulario.querySelector('#data').value
-
+    var inputTipoTransacao = elementoFormulario.querySelector('#tipoTransacao');
+    var inputValor = elementoFormulario.querySelector('#valor');
+    var inputData = elementoFormulario.querySelector('#data');
+    var tipoTransacao = inputTipoTransacao.value;
+    var valor = inputValor.valueAsNumber; // Necessário para não concatenar com string
+    var data = new Date(inputData.value); // Necessário para não tratar data como string
     if (tipoTransacao == 'Depósito') {
         saldo += valor;
-    } else if (tipoTransacao == 'Transferência' || tipoTransacao == 'Pagamento de Boleto') {
-        saldo -= valor;
-    } else {
-        alert('Tipo de transação inválida');
-        return
     }
-
-    elementoSaldo.textContent = saldo;
-
-    const novaTransacao = {
+    else if (tipoTransacao == 'Transferência' || tipoTransacao == 'Pagamento de Boleto') {
+        saldo -= valor;
+    }
+    else {
+        alert('Tipo de transação inválida');
+        return;
+    }
+    elementoSaldo.textContent = saldo.toString(); // Garante que o elemento tipo texto receba um texto
+    var novaTransacao = {
         tipoTransacao: tipoTransacao,
         valor: valor,
         data: data
-    }
-
+    };
     console.log(novaTransacao);
-
     elementoFormulario.reset(); //Limpar os dados do formulário
 });
+// Usar > tsc [nomeDoArquivo.ts] sempre que atualizá-lo
---
+++ js/bytebank.ts
@@ -0,0 +1,48 @@
+let saldo = 3000;
+
+const elementoSaldo = document.querySelector('.saldo-valor .valor') as HTMLElement; // Converte o valor para que o código saiba que se trata de um elemento HTML
+const elementoFormulario = document.querySelector('.block-nova-transacao form') as HTMLFormElement;
+
+if (elementoSaldo != null) { // Garante que o elemento existe no código
+    elementoSaldo.textContent = saldo.toString();
+}
+
+elementoFormulario.addEventListener('submit', function(event) {
+    event.preventDefault(); // Bloqueia o comportamento padrão de recarregar a página ao enviar o formulário
+
+    if (!elementoFormulario.checkValidity()) {
+        alert('Por favor, preeencha todos os campos da transação!');
+        return
+    }
+
+    const inputTipoTransacao = elementoFormulario.querySelector('#tipoTransacao') as HTMLSelectElement;
+    const inputValor = elementoFormulario.querySelector('#valor') as HTMLInputElement;
+    const inputData = elementoFormulario.querySelector('#data') as HTMLInputElement;
+
+    let tipoTransacao: string = inputTipoTransacao.value;
+    let valor: number = inputValor.valueAsNumber; // Necessário para não concatenar com string
+    let data: Date = new Date(inputData.value); // Necessário para não tratar data como string
+
+    if (tipoTransacao == 'Depósito') {
+        saldo += valor;
+    } else if (tipoTransacao == 'Transferência' || tipoTransacao == 'Pagamento de Boleto') {
+        saldo -= valor;
+    } else {
+        alert('Tipo de transação inválida');
+        return
+    }
+
+    elementoSaldo.textContent = saldo.toString(); // Garante que o elemento tipo texto receba um texto
+
+    const novaTransacao = {
+        tipoTransacao: tipoTransacao,
+        valor: valor,
+        data: data
+    }
+
+    console.log(novaTransacao);
+
+    elementoFormulario.reset(); //Limpar os dados do formulário
+});
+
+// Usar > tsc [nomeDoArquivo.ts] sempre que atualizá-lo
