1412d47635748e49970dd0c94f2165a2d6459ccc
Camila Santiago <92113919+Camilay3@users.noreply.github.com>
2025-05-13 20:55:25 -0300

    Tratamento de erros

--- dist/js/components/nova-transacao-component.js
+++ dist/js/components/nova-transacao-component.js
@@ -2,23 +2,28 @@ import saldoComponent from "./saldo-component.js";
 import Conta from "../types/Conta.js";
 const elementoFormulario = document.querySelector('.block-nova-transacao form');
 elementoFormulario.addEventListener('submit', function (event) {
-    event.preventDefault(); // Bloqueia o comportamento padrão de recarregar a página ao enviar o formulário
-    if (!elementoFormulario.checkValidity()) {
-        alert('Por favor, preeencha todos os campos da transação!');
-        return;
+    try {
+        event.preventDefault(); // Bloqueia o comportamento padrão de recarregar a página ao enviar o formulário
+        if (!elementoFormulario.checkValidity()) {
+            alert('Por favor, preeencha todos os campos da transação!');
+            return;
+        }
+        const inputTipoTransacao = elementoFormulario.querySelector('#tipoTransacao');
+        const inputValor = elementoFormulario.querySelector('#valor');
+        const inputData = elementoFormulario.querySelector('#data');
+        let tipoTransacao = inputTipoTransacao.value;
+        let valor = inputValor.valueAsNumber; // Necessário para não concatenar com string
+        let data = new Date(inputData.value); // Necessário para não tratar data como string
+        const novaTransacao = {
+            tipoTransacao: tipoTransacao,
+            data: new Date(),
+            valor: 0
+        };
+        Conta.registrarTransacao(novaTransacao);
+        saldoComponent.atualizar();
+        elementoFormulario.reset(); //Limpar os dados do formulário
     }
-    const inputTipoTransacao = elementoFormulario.querySelector('#tipoTransacao');
-    const inputValor = elementoFormulario.querySelector('#valor');
-    const inputData = elementoFormulario.querySelector('#data');
-    let tipoTransacao = inputTipoTransacao.value;
-    let valor = inputValor.valueAsNumber; // Necessário para não concatenar com string
-    let data = new Date(inputData.value); // Necessário para não tratar data como string
-    const novaTransacao = {
-        tipoTransacao: tipoTransacao,
-        data: new Date(),
-        valor: 0
-    };
-    Conta.registrarTransacao(novaTransacao);
-    saldoComponent.atualizar();
-    elementoFormulario.reset(); //Limpar os dados do formulário
+    catch (erro) {
+        alert(erro.message);
+    }
 });
--- src/components/nova-transacao-component.ts
+++ src/components/nova-transacao-component.ts
@@ -17,29 +17,33 @@ import saldoComponent from "./saldo-component.js";
 import Conta from "../types/Conta.js";
 
 const elementoFormulario = document.querySelector('.block-nova-transacao form') as HTMLFormElement;
-elementoFormulario.addEventListener('submit', function(event) {
-    event.preventDefault(); // Bloqueia o comportamento padrão de recarregar a página ao enviar o formulário
+elementoFormulario.addEventListener('submit', function (event) {
+    try {
+        event.preventDefault(); // Bloqueia o comportamento padrão de recarregar a página ao enviar o formulário
 
-    if (!elementoFormulario.checkValidity()) {
-        alert('Por favor, preeencha todos os campos da transação!');
-        return
+        if (!elementoFormulario.checkValidity()) {
+            alert('Por favor, preeencha todos os campos da transação!');
+            return
+        }
+
+        const inputTipoTransacao = elementoFormulario.querySelector('#tipoTransacao') as HTMLSelectElement;
+        const inputValor = elementoFormulario.querySelector('#valor') as HTMLInputElement;
+        const inputData = elementoFormulario.querySelector('#data') as HTMLInputElement;
+
+        let tipoTransacao: TipoTransacao = inputTipoTransacao.value as TipoTransacao;
+        let valor: number = inputValor.valueAsNumber; // Necessário para não concatenar com string
+        let data: Date = new Date(inputData.value); // Necessário para não tratar data como string
+
+        const novaTransacao: Transacao = {
+            tipoTransacao: tipoTransacao,
+            data: new Date(),
+            valor: 0
+        };
+
+        Conta.registrarTransacao(novaTransacao);
+        saldoComponent.atualizar();
+        elementoFormulario.reset(); //Limpar os dados do formulário
+    } catch (erro) {
+        alert(erro.message)
     }
-
-    const inputTipoTransacao = elementoFormulario.querySelector('#tipoTransacao') as HTMLSelectElement;
-    const inputValor = elementoFormulario.querySelector('#valor') as HTMLInputElement;
-    const inputData = elementoFormulario.querySelector('#data') as HTMLInputElement;
-
-    let tipoTransacao: TipoTransacao = inputTipoTransacao.value as TipoTransacao;
-    let valor: number = inputValor.valueAsNumber; // Necessário para não concatenar com string
-    let data: Date = new Date(inputData.value); // Necessário para não tratar data como string
-
-    const novaTransacao: Transacao = {
-        tipoTransacao: tipoTransacao,
-        data: new Date(),
-        valor: 0
-    };
-
-    Conta.registrarTransacao(novaTransacao);
-    saldoComponent.atualizar();
-    elementoFormulario.reset(); //Limpar os dados do formulário
 });
