25853278410cc6e6554e8a317b6626681b109387
Camila Santiago <92113919+Camilay3@users.noreply.github.com>
2025-05-14 21:18:39 -0300

    Iniciando histórico de transações

--- src/types/Conta.ts
+++ src/types/Conta.ts
@@ -1,5 +1,6 @@
 import { Transacao } from "./Transacao.js";
 import { TipoTransacao } from "./TipoTransacao.js";
+import { GrupoTransacao } from "./GrupoTransacao.js";
 
 let saldo: number = JSON.parse(localStorage.getItem("saldo")) || 0;
 const transacoes: Transacao[] = JSON.parse(localStorage.getItem("transacoes"), (key: string, value: string) => {
@@ -34,6 +35,29 @@ const Conta = {
 
     getDataAcesso(): Date {
         return new Date();
+    },
+
+    getGrupoTransacoes(): GrupoTransacao[] {
+        const grupoTransacoes: GrupoTransacao[] = [];
+        const listaTransacoes: Transacao[] = structuredClone(transacoes); // Copia a estrutura do objeto - Evita falhas de segurança na lista original
+        const transacoesOrdenadas: Transacao[] = listaTransacoes.sort((t1, t2) => t2.data.getTime() - t1.data.getTime());
+        let labelAtualGrupoTransacao: string = "";
+
+        // Agrupa o histórico de transações por mês, mesmo sendo feitas fora de ordem
+        for (let transacao of transacoesOrdenadas) {
+            let labelGrupoTransacao:string = transacao.data.toLocaleDateString('pt-br', {month: "long", year:"numeric"});
+            
+            if (labelAtualGrupoTransacao != labelGrupoTransacao) {
+                labelAtualGrupoTransacao = labelGrupoTransacao;
+                grupoTransacoes.push({
+                    label: labelAtualGrupoTransacao,
+                    transacoes: []
+                })
+            }
+            grupoTransacoes.at(-1).transacoes.push(transacao)
+        }
+
+        return grupoTransacoes;
     },
 
     registrarTransacao(novaTransacao: Transacao): void {
